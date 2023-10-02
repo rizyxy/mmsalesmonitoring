b3a5878cfd6c21aef84c981ad24a3204c8cf927b
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-10-03 01:34:39 +0700

    Added Dashboard

--- master/views.py
+++ master/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
-# Create your views here.
+def dashboard(request):
+    template = loader.get_template('dashboard.html')
+    context= {}
+    return HttpResponse(template.render(context, request))
