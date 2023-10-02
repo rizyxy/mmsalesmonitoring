595536bf1a1d5ccf846283c84466619a0103ef4b
Mukhammad Rizki <rizkyRE7@gmail.com>
2023-10-02 17:32:34 +0700

    Added All Products Page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from master.models import Product
 
-# Create your views here.
+def all_products(request):
+    products = Product.objects.all().values()
+    template = loader.get_template('all_products.html')
+    context = {
+        'products': products
+    }
+
+    return HttpResponse(template.render(context, request))
